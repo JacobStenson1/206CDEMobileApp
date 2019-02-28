65578f8c3df34d2b6dd9bbda06c9a8cb19215f8f
Jacob Stenson <jacob@nosnets.co.uk>
2019-02-28 17:28:32 +0000

    Added HTML code for the logindata.php
    Added HTML code which contains PHP for the printing of the data.
    This is required to make sure e 'get marks' and the code is 'syntactically correct'

--- LoginData.php
+++ LoginData.php
@@ -17,13 +17,21 @@
   $sql = "SELECT Username, Password FROM userlogindata";
   $result = mysqli_query($conn, $sql);
 
-  if(mysqli_num_rows($result) > 0)
-  {
-    // Show data for each row
-    while($row = mysqli_fetch_assoc($result))
-    {
-      echo "|Username:".$row['Username']."|Password:".$row['Password'].";";
-    }
-  }
+
 
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head></head>
+ <body>
+   <?php if(mysqli_num_rows($result) > 0)
+   {
+     // Show data for each row
+     while($row = mysqli_fetch_assoc($result))
+     {
+       echo "|Username:".$row['Username']."|Password:".$row['Password'].";";
+     }
+   } ?>
+ </body>
+ </html>
